cf903537da890b89c5191039feb915a172b43676
Cameron Ellis <cameronellis.ellis@gmail.com>
2022-08-03 21:25:35 -0700

    Update lpad to use const

--- lib/util/lpad.ts
+++ lib/util/lpad.ts
@@ -1,6 +1,6 @@
 export default function(str: string, padChar: string, totalLength: number) {
   str = str.toString();
-  var neededPadding = totalLength - str.length;
+  const neededPadding = totalLength - str.length;
   for (let i = 0; i < neededPadding; i++) {
     str = padChar + str;
   }
